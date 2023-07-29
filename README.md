7c68cb9d2bdbe5f50edc8c8d7a0647225afcc8d3
Xunhaoz <leo20020529@gmail.com>
2023-07-30 01:07:41 +0800

    first release

---
+++ README.md
@@ -0,0 +1,55 @@
+# Folder and File List Generator
+
+A Python script that generates a hierarchical list of folders and files within a specified directory, excluding items
+listed in the .gitignore file and the script file itself.
+
+## How to Use
+
+1. Clone the repository to your local machine.
+2. Make sure you have Python installed.
+3. Navigate to the project directory in the terminal.
+
+## Usage
+
+python folder_file_list.py
+
+## Output
+
+The script will generate a file named "structure.md" in the project directory, containing the folder and file list in a
+hierarchical format.
+
+```
+├── README.md
+├── test
+|	├── test_1
+|	|	├── test_1_1
+|	|	|	├── 123.py
+|	|	|	├── 456.py
+|	|	|	├── 789.py
+|	|	|	└── __init__.py
+|	|	└── __init__.py
+|	└── __init__.py
+├── test1
+|	├── test1_1
+|	└── test1_2
+└── test2
+	└── 123.py
+```
+
+## Configuration
+
+By default, the script will not display debugging information. If you want to see the output as the script processes,
+set the `debug` parameter to True when creating the Lister object.
+
+```python
+lister = Lister(debug=True)
+```
+
+## .gitignore
+
+The script takes into account the .gitignore file present in the project directory, excluding the listed folders and
+files from the generated structure.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ folder_file_list.py
@@ -0,0 +1,55 @@
+import os
+
+
+class Lister:
+    def __init__(self, debug=False):
+        self._tail = []
+        self._ignore_folders_files = []
+        self._get_git_ignore()
+        self._debug = debug
+        self._console = ""
+
+    def list_folders_and_files(self, root='.', deep=0):
+        ls = os.listdir(root)
+        ls = list(filter(lambda x: x not in self._ignore_folders_files, ls))
+
+        self._tail.append(1)
+        for key, item in enumerate(ls):
+            pre = ['|\t' if _ == 1 else '\t' for _ in self._tail[:-1]]
+            pre = ''.join(pre)
+
+            if key + 1 == len(ls):
+                output = pre + '└── ' + item + '\n'
+                self._tail[deep] = 0
+            else:
+                output = pre + '├── ' + item + '\n'
+            self._console += output
+
+            if self._debug:
+                print(output, end='')
+
+            if os.path.isdir(os.path.join(root, item)):
+                self.list_folders_and_files(root=os.path.join(root, item), deep=deep + 1)
+                self._tail = self._tail[:-1]
+
+    def _get_git_ignore(self):
+        if os.path.exists('.gitignore'):
+            with open('.gitignore') as f:
+                ignore_folders_files = f.readlines()
+                ignore_folders_files = filter(lambda x: x != '\n', ignore_folders_files)
+                ignore_folders_files = [x.replace('\n', '') for x in ignore_folders_files]
+            ignore_folders_files.append('.gitignore')
+            self._ignore_folders_files = ignore_folders_files
+        self._ignore_folders_files.append(os.path.basename(__file__))
+
+    def dump(self, path='structure.md'):
+        with open(path, 'w', encoding='UTF-8') as f:
+            f.write('```\n')
+            f.write(self._console)
+            f.write('```')
+
+
+if __name__ == "__main__":
+    lister = Lister()
+    lister.list_folders_and_files()
+    lister.dump()
